8184f45a8f1b2a93171ed9e59e73ef5df4a59fe0
José Alexander Bautista Gálvez <jsalexchota@gmail.com>
2024-11-28 10:47:23 -0500

    Demostracion de hook formato de codigo

--- DemoHooks/Program.cs
+++ DemoHooks/Program.cs
@@ -1,2 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+                                                Console.WriteLine("Prueba de formateo de Codigo");
+                                                                                                                                                int edad = 0;
+Console.ReadLine();
+
+if                                                                                                                  (edad >= 18)
+                                                                                {
+                                 Console.WriteLine("Es mayor de edad");
+}
+else
+                                                    {
+                                                                                                            Console.WriteLine("Es menor de edad");
+}
